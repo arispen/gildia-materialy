e691144d654fc1b19d563873063af41da31dfa67
Maciej Wróblewski <arispen@gmail.com>
2022-10-26 10:43:48 +0000

    extract player

--- wiedzmak/main.lua
+++ wiedzmak/main.lua
@@ -1,3 +1,5 @@
+require("wiedzmak/player")
+
 function love.load()
   tileSize = 32
   map = { size = 512 }
@@ -6,8 +8,8 @@ function love.load()
   love.graphics.setDefaultFilter("nearest")
   map.image = love.graphics.newImage("wiedzmak/map.png")
   
-  wiedzmak = { x = tileSize * 3, y = tileSize * 3} 
-  wiedzmak.image = love.graphics.newImage("wiedzmak/wiedzmak.png")
+  player = { x = tileSize * 4, y = tileSize * 3} 
+  player.image = love.graphics.newImage("wiedzmak/wiedzmak.png")
 end
 
 function love.update(dt)
@@ -15,27 +17,11 @@ function love.update(dt)
 end
 
 function love.keypressed(key, scancode, isrepeat)
-	if key == "right" then
-    if wiedzmak.x + tileSize < map.size - tileSize * 3 then
-      wiedzmak.x = wiedzmak.x + tileSize
-    end
-	elseif key == "left" then
-		if wiedzmak.x - tileSize > tileSize * 2 then
-      wiedzmak.x = wiedzmak.x - tileSize
-    end
-  elseif key == "up" then
-		if wiedzmak.y - tileSize > tileSize * 2 then
-      wiedzmak.y = wiedzmak.y - tileSize
-    end
-  elseif key == "down" then
-		if wiedzmak.y + tileSize < map.size - tileSize * 3 then
-      wiedzmak.y = wiedzmak.y + tileSize
-    end
-	end
+  movePlayer(key, player, map, tileSize)
 end
 
 
 function love.draw()
   love.graphics.draw(map.image, 0, 0, 0, 2, 2)
-  love.graphics.draw(wiedzmak.image, wiedzmak.x, wiedzmak.y, 0, 2, 2)
+  love.graphics.draw(player.image, player.x, player.y, 0, 2, 2)
 end
---
+++ wiedzmak/player.lua
@@ -0,0 +1,20 @@
+
+function movePlayer(key, player, map, tileSize)
+	if key == "right" then
+    if player.x + tileSize < map.size - tileSize * 3 then
+      player.x = player.x + tileSize
+    end
+	elseif key == "left" then
+		if player.x - tileSize > tileSize * 2 then
+      player.x = player.x - tileSize
+    end
+  elseif key == "up" then
+		if player.y - tileSize > tileSize * 2 then
+      player.y = player.y - tileSize
+    end
+  elseif key == "down" then
+		if player.y + tileSize < map.size - tileSize * 3 then
+      player.y = player.y + tileSize
+    end
+	end
+end
